1e842d728f0e2ec4585b65a2aa71f2c265662804
Sai Karthik R <55224382+SaiKarthik2000@users.noreply.github.com>
2022-03-08 11:42:02 +0530

    Add files via upload

--- milestone1a.py
+++ milestone1a.py
@@ -12,25 +12,25 @@ def printlog(task,path):
     print(datetime.now(),";",path," Exit",sep='')
 def task_execution(data,path,ex):
     for i in range(len(data)):
-        path=path+'.'+list(data.keys())[i]
-        for task in data:
-            if ex=="Concurrent":
-                _thread.start_new_thread(printlog(task,path))
-            else:
-                printlog(task,path)
+        path1=path
+        data1=data
+        path1=path+'.'+list(data.keys())[i]
+        data1=data[list(data.keys())[i]]
+        if ex=="Concurrent":
+            _thread.start_new_thread(printlog(data1,path1))
+        else:
+            printlog(data1,path1)
 
 def flow_execution(data,path,n):
     ex=""
     if n==0:
         path=path+list(data.keys())[0]
+        data=data[list(data.keys())[0]]
     else:
         ex=data["Execution"]
         data=data["Activities"]
-        path=path+'.'+list(data.keys())[0]
-    data=data[list(data.keys())[0]]
-    if(data["Type"]=="Task"):
         task_execution(data,path,ex)
-    else:
+    if(data["Type"]=="Flow"):
         printlog(data,path)
 
 with open('G:\College\Code\KLA\Milestone1A.yaml') as ms1a:
